298e020fbabe0ac9b5efb882350aa113871566e9
hanruiyao163 <hanruiyao163@163.com>
2023-11-20 14:40:37 +0800

    构建pwsh和bash执行脚本 一键启动+++

--- ruoyi-ui/pwsh-run.ps1
+++
@@ -1,4 +0,0 @@
-npm config set script-shell "C:/Program Files/PowerShell/7/pwsh.exe";
-npm config set shell "C:/Program Files/PowerShell/7/pwsh.exe";
-$env:NODE_OPTIONS='--openssl-legacy-provider';
-vue-cli-service serve
