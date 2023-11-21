685e70645894286853d3fd80181ff8d22bef998b
lingnanbai <32591542+bailingnan@users.noreply.github.com>
2023-11-21 08:45:44 +0800

    bugFix:服务器IP地址变化时修改sys_oss表的url字段和sys_oss_config表的endpoint字段

--- ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysOssConfigMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/SysOssConfigMapper.java
@@ -3,6 +3,7 @@ package com.ruoyi.system.mapper;
 import com.ruoyi.common.core.mapper.BaseMapperPlus;
 import com.ruoyi.system.domain.SysOssConfig;
 import com.ruoyi.system.domain.vo.SysOssConfigVo;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 对象存储配置Mapper接口
@@ -12,5 +13,5 @@ import com.ruoyi.system.domain.vo.SysOssConfigVo;
  * @date 2021-08-13
  */
 public interface SysOssConfigMapper extends BaseMapperPlus<SysOssConfigMapper, SysOssConfig, SysOssConfigVo> {
-
+        int updateEndPoint(@Param("oldEndPoint")String oldEndPoint, @Param("newEndPoint")String newEndPoint);
 }
