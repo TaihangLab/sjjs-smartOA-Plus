af45c0c42ad64c4859bec35170f7295b818612a0
xuzhe <xz96424@163.com>
2023-11-24 15:38:10 +0800

    项目列表表单构建

--- ruoyi-ui/src/views/project/Project.vue
+++ ruoyi-ui/src/views/project/Project.vue
@@ -4,7 +4,9 @@
             <!-- 角色数据表单 -->
             <el-table ref="multipleTable" :data="dataList" border style="width: 100%" :row-style="{height: '50px'}"
                       :cell-style="{padding:'0px'}">
-                <!--        <el-table-column type="selection" width="39"></el-table-column>-->
+                <el-table-column type="selection" resizable="false" align="center" width="40"></el-table-column>
+                <el-table-column label="#" :resizable="false" align="center"  prop="projectId" width="80">
+                </el-table-column>
                 <el-table-column label="项目编号" :resizable="false" align="center"  prop="projectId" width="80">
                 </el-table-column>
                 <el-table-column label="项目名称" :resizable="false" align="center"  prop="project.name" width="150">
