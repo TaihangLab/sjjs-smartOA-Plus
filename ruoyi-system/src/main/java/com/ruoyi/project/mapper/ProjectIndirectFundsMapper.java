fb7c42f41efb728972a0c4718d8d573d5e3ff5c9
lingnanbai <32591542+bailingnan@users.noreply.github.com>
2023-12-05 16:08:47 +0800

    feature:新建project_funds和project_indirect_funds实体类，mapper及xml文件

---
+++ ruoyi-system/src/main/java/com/ruoyi/project/mapper/ProjectIndirectFundsMapper.java
@@ -0,0 +1,19 @@
+package com.ruoyi.project.mapper;
+
+import com.ruoyi.common.core.mapper.BaseMapperPlus;
+import com.ruoyi.project.domain.ProjectIndirectFunds;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author bailingnan
+* @description 针对表【project_indirect_funds(项目间接费用表)】的数据库操作Mapper
+* @createDate 2023-12-05 15:59:43
+* @Entity com.ruoyi.project.domain.ProjectIndirectFunds
+*/
+public interface ProjectIndirectFundsMapper extends BaseMapperPlus<ProjectIndirectFundsMapper,ProjectIndirectFunds,ProjectIndirectFunds> {
+
+}
+
+
+
+
