9b8f9888d8f026571fc186527d13c82541edd2ef
fanjiaxing <c1111111c@163.com>
2023-12-06 17:51:14 +0800

    新增projectuser对应的实体类，mapper，service

---
+++ ruoyi-system/src/main/java/com/ruoyi/project/mapper/ProjectUserMapper.java
@@ -0,0 +1,8 @@
+package com.ruoyi.project.mapper;
+
+import com.ruoyi.common.core.mapper.BaseMapperPlus;
+import com.ruoyi.project.domain.ProjectUser;
+
+public interface ProjectUserMapper extends BaseMapperPlus<ProjectUserMapper, ProjectUser,ProjectUser> {
+
+}
