fe2b00c3a6d6d1220a91f6e28fd4f906961e2d96
fanjiaxing <c1111111c@163.com>
2023-12-14 10:03:28 +0800

    大事记增加校验注解

--- ruoyi-system/src/main/java/com/ruoyi/project/domain/ProjectMilestone.java
+++ ruoyi-system/src/main/java/com/ruoyi/project/domain/ProjectMilestone.java
@@ -19,7 +19,6 @@ public class ProjectMilestone {
     /**
      * 大事纪ID
      */
-    @TableId(value = "milestone_id",type = IdType.AUTO)
     private Long milestoneId;
 
     /**
@@ -30,22 +29,16 @@ public class ProjectMilestone {
     /**
      * 大事记名称
      */
-    @Xss(message = "大事记名称不能包含脚本字符")
-    @NotBlank(message = "大事记名称不能为空")
-    @Size(min = 0, max = 50, message = "大事记名称不能超过{max}个字符")
     private String milestoneTitle;
 
     /**
      * 大事记描述
      */
-    @NotBlank(message = "大事记描述不能为空")
-    @Size(min = 0, max = 100, message = "大事记描述不能超过{max}个字符")
     private String milestoneRemark;
 
     /**
      * 大事记时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate milestoneDate;
 
 }
