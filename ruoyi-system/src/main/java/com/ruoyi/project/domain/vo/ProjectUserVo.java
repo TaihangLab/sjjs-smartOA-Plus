b48d0bfac0ca98a69e03789702008199cdf4bf1f
fanjiaxing <c1111111c@163.com>
2023-12-21 10:28:30 +0800

    新增用户角色枚举类（BO引用）

--- ruoyi-system/src/main/java/com/ruoyi/project/domain/vo/ProjectUserVo.java
+++ ruoyi-system/src/main/java/com/ruoyi/project/domain/vo/ProjectUserVo.java
@@ -32,7 +32,7 @@ public class ProjectUserVo extends BaseEntity {
     private String deptName;
 
     /**
-     * 项目成员角色
+     * 项目成员角色，0：项目负责人；1：公司负责人；2：部门负责人；3：科研管理负责人；4：普通成员
      */
     private ProjectUserRole projectUserRole;
 }
