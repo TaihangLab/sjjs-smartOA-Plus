980aba7cea3b67d8310e5af1b23b56267461ec28
xuzhe <xz96424@163.com>
2023-12-21 09:27:27 +0800

    详情页界面调整

--- ruoyi-ui/src/views/project/components/ViewProject/Member.vue
+++ ruoyi-ui/src/views/project/components/ViewProject/Member.vue
@@ -6,6 +6,7 @@
             <el-table-column label="邮箱" align="center" key="email" prop="email" v-if="columns[1].visible" :show-overflow-tooltip="true" />
             <el-table-column label="手机号码" align="center" key="phonenumber" prop="phonenumber" v-if="columns[2].visible" :show-overflow-tooltip="true" />
             <el-table-column label="所属部门" align="center" key="deptName" prop="deptName" v-if="columns[3].visible" width="120" />
+            <el-table-column label="项目角色" align="center"  v-if="columns[4].visible" width="120" />
         </el-table>
     </div>
 </template>
@@ -13,6 +14,9 @@
 
 import request from '@/utils/request';
 export default {
+    props: {
+        projectid: String,
+    },
     name: "User",
     data() {
         return {
@@ -21,40 +25,48 @@ export default {
             // 用户表格数据
             userList: [],
             // 查询参数
-            projectId: 11,
+            projectId: 0,
 
             // 列信息
             columns: [
-                { key: 0, label: `用户编号`, visible: true },
-                { key: 1, label: `用户名称`, visible: true },
-                { key: 2, label: `用户昵称`, visible: true },
-                { key: 3, label: `部门`, visible: true },
+                { key: 0, label: `用户名称`, visible: true },
+                { key: 1, label: `邮箱`, visible: true },
+                { key: 2, label: `电话号码`, visible: true },
+                { key: 3, label: `所属部门`, visible: true },
+                { key: 4, label: `项目角色`, visible: true },
             ],
         };
     },
     created() {
-
+        this.projectpepole();
     },
-    mounted() {
-        console.log("传过来的项目id", this.projectId);
-        // 使用正确的用户列表接口，假设接口为 /user/list
-        request({
-            url: '/project/list/projectmembers',
-            method: 'get',
-            params: {
-                projectId: this.projectId
-            },
-        })
-            .then((resp) => {
-                // 处理获取的用户数据
-                console.log('Response Data:', resp.data);
-                this.userList = resp.data;
-                this.loading = false; // 关闭 loading 遮罩层
+    watch: {
+        // 监听 projectid 的变化，一旦变化就调用 projectpepole 方法
+        projectid: 'projectpepole',
+    },
+    methods:{
+        projectpepole(){
+            console.log("传过来的项目123", this.$props.projectid);
+            // 使用正确的用户列表接口，假设接口为 /user/list
+            request({
+                url: '/project/list/getDetails',
+                method: 'get',
+                params: {
+                    projectId: this.$props.projectid,
+                },
             })
-            .catch((error) => {
-                console.error('获取用户数据时出错：', error);
-                this.loading = false; // 处理错误，关闭 loading 遮罩层
-            });
+                .then((resp) => {
+                    // 处理获取的用户数据
+                    console.log('Response Data:', resp);
+                    this.userList = resp.data.projectUserVoList;
+                    this.loading = false; // 关闭 loading 遮罩层
+                })
+                .catch((error) => {
+                    console.error('获取用户数据时出错：', error);
+                    this.loading = false; // 处理错误，关闭 loading 遮罩层
+                });
+            console.log("项目成员", resp.projectUserVoList);
+        },
     },
 };
 </script>
