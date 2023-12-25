a2f17de5b80db44f96b015d0f0f63a108fc82d4b
xuzhe <xz96424@163.com>
2023-12-25 16:50:49 +0800

    项目计划显示

--- ruoyi-ui/src/views/project/components/ViewProject/ProjectPlan.vue
+++ ruoyi-ui/src/views/project/components/ViewProject/ProjectPlan.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div v-if="$props.projectplan && $props.projectplan.length > 0">
         <div style="margin-top: 10px;"></div>
         <div v-for="(plan, index) in $props.projectplan" :key="index">
             <el-descriptions class="margin-top" :column="2" border>
@@ -18,8 +18,11 @@
             </el-descriptions>
             <div style="margin-bottom: 20px;">
                 <hr style="margin: 20px 0; border: 1px solid #ccc;">
-            </div> <!-- 添加间隔 -->
+            </div>
         </div>
+    </div>
+    <div v-else class="no-data-message" style="color: #909399; font-size: 14px; text-align: center;">
+        暂无数据
     </div>
 </template>
 
