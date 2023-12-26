12c1544cbe546f86aaca568ae17e455a1ba12164
123 <1749750148@qq.com>
2023-12-26 18:03:30 +0800

    项目详情附件

---
+++ ruoyi-ui/src/views/project/components/ViewProject/Projectfile.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <div style="margin-top: 10px;"></div>
+        <el-table v-loading="loading" :data="transformedfile" style="margin: 0; padding: 0;">
+            <!-- el-table-column 改用插值语法传递 label -->
+            <el-table-column :label="'文件名称'" align="center" prop="originalName" :show-overflow-tooltip="true" />
+            <el-table-column :label="'文件格式'" align="center" prop="fileSuffix" :show-overflow-tooltip="true" >
+                <template slot-scope="scope">
+                    <div>
+                        <el-tag type="success" size="mini">{{ scope.row.fileSuffix }}</el-tag>
+                    </div>
+                </template>
+            </el-table-column>
+            <el-table-column :label="'上传人'" align="center" prop="createBy" :show-overflow-tooltip="true" />
+            <el-table-column :label="'上传时间'" align="center" prop="createTime" :show-overflow-tooltip="true" />
+            <el-table-column :label="'操作'" align="center">
+                <template slot-scope="scope">
+                    <el-link :href="scope.row.url" target="_blank" :underline="false" class="attachment-item">
+                        <i class="el-icon-download custom-icon"></i>
+                    </el-link>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+import request from '@/utils/request';
+import data from "../../../system/dict/data.vue";
+export default {
+    props: {
+        projectfile: {
+            type: Object,
+            required: true,
+        },
+    },
+    name: "file",
+    data() {
+        return {
+
+        };
+    },
+    watch: {
+        projectfile: {
+            handler() {
+                // 在属性变化时调用获取详情的方法
+                this.transformedfile();
+            },
+            deep: true,
+        },
+    },
+    methods: {
+
+    },
+    computed: {
+        data() {
+            return data
+        },
+        transformedfile() {
+            // 对用户列表进行数据重构
+            const result = [];
+            this.projectfile.forEach((file) => {
+                const cleanedName = file.originalName.replace(/\.[^/.]+$/, '');
+                // 处理上传时间，只保留到日
+                const createTime = new Date(file.createTime).toLocaleDateString();
+                result.push({
+                    originalName: cleanedName,
+                    fileSuffix: file.fileSuffix,
+                    createBy: file.createBy,
+                    createTime: createTime,
+                    url: file.url,
+                    ossId: file.ossId,
+                    // projectUserRole: file.role,
+                });
+            });
+            return result;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.custom-icon {
+    font-size: 20px; /* 设置图标的字体大小 */
+}
+</style>
