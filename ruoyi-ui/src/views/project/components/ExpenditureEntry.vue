d69fc68d1e5bc37e64ac710161be211d5ffefa42
xz <xz96424@163.com>
2024-03-05 10:05:22 +0800

    支出录入中文件导入之后数据显示在列表中

--- ruoyi-ui/src/views/project/components/ExpenditureEntry.vue
+++ ruoyi-ui/src/views/project/components/ExpenditureEntry.vue
@@ -10,39 +10,44 @@
                     v-hasPermi="['system:user:import']">导入</el-button>
             </el-col>
         </el-row>
-        <el-table ref="multipleTable" :data="this.intellectualLook.ipUserVOList" border style="width: 100%"
-            :row-style="{ height: '50px' }" :cell-style="{ padding: '0px' }">
-            <el-table-column label="年" :resizable="false" align="center" prop="" width="50px">
+        <el-table ref="multipleTable" :data="da" border style="width: 100%" :row-style="{ height: '50px' }"
+            :cell-style="{ padding: '0px' }">
+            <el-table-column label="日期" :resizable="false" align="center">
+                <!-- 使用 slot-scope 定制显示日期 -->
+                <template slot-scope="scope">
+                    {{ formatDate(scope.row.expenditureDate) }}
+                </template>
             </el-table-column>
-            <el-table-column label="月" :resizable="false" align="center" prop="" width="50px">
+            <el-table-column label="项目名称" :resizable="false" align="center" prop="projectName" width="250px">
             </el-table-column>
-            <el-table-column label="日" :resizable="false" align="center" prop="" width="50px">
+            <el-table-column label="凭证号" :resizable="false" align="center" prop="voucherNo" width="100px">
             </el-table-column>
-            <el-table-column label="日期" :resizable="false" align="center" prop="" width="100px">
+            <el-table-column label="摘要" :resizable="false" align="center" prop="expenditureAbstract" min-width="400px">
             </el-table-column>
-            <el-table-column label="项目名称" :resizable="false" align="center" prop="jobTitle" :formatter="jobTitles"
-                width="250px">
+            <el-table-column label="专项/自筹" :resizable="false" align="center" prop="zxzc" :formatter="zxzcFormatter">
             </el-table-column>
-            <el-table-column label="凭证号" :resizable="false" align="center" prop="email" width="100px">
+            <el-table-column label="科目粗分" :resizable="false" align="center" prop="firstLevelSubject"
+                :formatter="firstLevelSubjectFormatter">
             </el-table-column>
-            <el-table-column label="摘要" :resizable="false" align="center" prop="phonenumber" min-width="400px">
+            <el-table-column label="科目细分" :resizable="false" align="center" prop="secondLevelSubject"
+                :formatter="secondLevelSubjectFormatter">
             </el-table-column>
-            <el-table-column label="专项/自筹" :resizable="false" align="center" prop="diploma" :formatter="diplomas">
-            </el-table-column>
-            <el-table-column label="科目粗分" :resizable="false" align="center" prop="deptName">
-            </el-table-column>
-            <el-table-column label="科目细分" :resizable="false" align="center" prop="deptName">
-            </el-table-column>
-            <el-table-column label="金额" :resizable="false" align="center" prop="phonenumber" width="150px">
+            <el-table-column label="金额" :resizable="false" align="center" prop="amount" width="150px">
             </el-table-column>
         </el-table>
         <!--新增支出录入-->
-        <el-dialog title="信息录入" :visible.sync="ExpenditureAdd" width="700px" append-to-body>
+        <el-dialog title="信息录入" :visible.sync="ExpenditureAdd" width="700px" max-hight="400px" append-to-body>
             <ExpenditureAdd @close-dialog="closeExpenditureDialog"></ExpenditureAdd>
         </el-dialog>
         <!--导入支出录入表-->
         <el-dialog title="信息导入" :visible.sync="ExpenditureImport" width="400px" append-to-body>
-            <ExpenditureImport @close-dialog="closeExpenditureDialog"></ExpenditureImport>
+            <el-upload class="upload-demo" action="" :on-change="handleChange" :on-remove="handleRemove"
+                :on-exceed="handleExceed"
+                accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel"
+                :auto-upload="false">
+                <!-- 只 能 上 传 xlsx / xls 文 件 -->
+                <el-button size="small" type="primary">点击上传</el-button>
+            </el-upload>
         </el-dialog>
     </div>
 </template>
@@ -50,10 +55,9 @@
 <script>
 import request from "@/utils/request";
 import ExpenditureAdd from "@/views/project/components/ExpenditureAdd.vue";
-import ExpenditureImport from "@/views/project/components/ExpenditureImport.vue";
 
 export default {
-    components: { ExpenditureAdd, ExpenditureImport },
+    components: { ExpenditureAdd },
     props: {
         ipId: {
             type: [Number, String],
@@ -62,20 +66,6 @@ export default {
     },
     data() {
         return {
-            jobTitle: {
-                0: '正高级工程师',
-                1: '副高级工程师',
-                2: '中级工程师',
-                3: '初级工程师',
-                4: '研究员',
-                5: '副研究员',
-                6: '助理研究员',
-                7: '研究实习员',
-            },
-            diploma: {
-                0: '专项',
-                1: '自筹',
-            },
             contentStyle: {
                 'text-align': 'center',
                 'width': '60%',
@@ -87,14 +77,16 @@ export default {
             loading: true,
             ExpenditureAdd: false,
             ExpenditureImport: false,
-            intellectualLook: {}, 
+            expenditureLook: [],
+            importedData: [],
+            da: [],
         };
     },
     watch: {
         ipId: {
             handler(newVal) {
                 this.params.ipId = newVal;
-                this.checkIntellectual();
+                // this.checkExpenditure();
             },
             immediate: true, // 立即执行一次
         },
@@ -105,13 +97,66 @@ export default {
             const lastDotIndex = originalName.lastIndexOf('.');
             return lastDotIndex !== -1 ? originalName.substring(0, lastDotIndex) : originalName;
         },
-        jobTitles(row, column, cellValue) {
-            // 使用映射关系来获取对应的文字描述
-            return this.jobTitle[cellValue] || cellValue;
+         // 格式化日期方法
+         formatDate(date) {
+            // 假设日期格式为 "YYYY-MM-DD"
+            const parts = date.split('-');
+            if (parts.length === 3) {
+                const year = parts[0];
+                const month = parts[1];
+                const day = parts[2];
+                return `${year}-${month}-${day}`;
+            }
+            // 如果日期格式不正确，直接返回原始日期
+            return date;
         },
-        diplomas(row, column, cellValue) {
-            // 使用映射关系来获取对应的文字描述
-            return this.diploma[cellValue] || cellValue;
+        // 科目粗分
+        firstLevelSubjectFormatter(row) {
+            const firstLevelSubject = {
+                0: '设备费',
+                1: '业务费',
+                2: '劳务费',
+                3: '材料费',
+                4: '科研活动费',
+                5: '科研服务费',
+                6: '人员和劳务补助费',
+                7: '绩效支出',
+            };
+            return firstLevelSubject[row.firstLevelSubject];
+        },
+        // 科目细分
+        secondLevelSubjectFormatter(row) {
+            const secondLevelSubject = {
+                0: '购置设备费',
+                1: '试制设备费',
+                2: '设备升级改造费',
+                3: '设备租赁费',
+                4: '材料费',
+                5: '资料费',
+                6: '数据样本采集费',
+                7: '测试化验加工费',
+                8: '燃料动力费',
+                9: '办公费',
+                10: '印刷/出版费',
+                11: '知识产权事务费',
+                12: '车辆使用费',
+                13: '出版/文献/信息传播/知识产权事务费',
+                14: '差旅费',
+                15: '会议/会务费',
+                16: '国内协作费',
+                17: '国际合作交流费',
+                18: '专家咨询费',
+                19: '人员劳务费',
+            };
+            return secondLevelSubject[row.secondLevelSubject];
+        },
+        // 专项自筹
+        zxzcFormatter(row) {
+            const zxzc = {
+                0: '专项',
+                1: '自筹',
+            };
+            return zxzc[row.zxzc];
         },
         /** 新增按钮操作 */
         handleAdd() {
@@ -124,24 +169,60 @@ export default {
         closeExpenditureDialog() {
             this.ExpenditureAdd = false;
             this.ExpenditureImport = false;
+
         },
-        checkIntellectual() {
-            this.params.ipId = this.$props.ipId;
-            request({
-                url: '/ip/getDetails',
-                method: 'get',
-                params: this.params,
+        limitUpload() {
+
+        },
+        //超出最大上传文件数量时的处理方法
+        handleExceed() {
+            this.$message({
+                type: 'warning',
+                message: '超出最大上传文件数量的限制！'
             })
-                .then((resp) => {
-                    this.intellectualLook = resp.data;
-                    this.loading = false; // 数据加载完成后隐藏遮罩层
-                })
-                .catch((error) => {
-                    console.error('获取数据时出错：', error);
-                    this.loading = false; // 在出错情况下也隐藏遮罩层
+            return;
+        },
+        //移除文件的操作方法
+        handleRemove(file, fileList) {
+            this.fileTemp = null
+        },
+        handleChange(file, fileList) {
+            this.fileTemp = file.raw;
+            if (this.fileTemp) {
+                if ((this.fileTemp.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+                    || (this.fileTemp.type == 'application/vnd.ms-excel')) {
+                    // 调用后端接口，上传数据
+                    const formData = new FormData();
+                    formData.append('file', file.raw);
+                    request.post('/project/funds/importData', formData)
+                        .then(response => {
+                            // 处理后端响应，将解析后的数据设置到列表中
+                            console.log('数据传入', response);
+                            this.da = response.data;
+                            this.$message({
+                                type: 'success',
+                                message: '文件上传成功！'
+                            });
+                        })
+                        .catch(error => {
+                            // 处理上传失败的情况
+                            console.error('文件上传失败：', error);
+                            this.$message.error('文件上传失败，请稍后重试！');
+                        });
+                } else {
+                    this.ExpenditureImport = false; // 文件格式错误时关闭导入对话框
+                    this.$message({
+                        type: 'warning',
+                        message: '文件格式错误，请删除后重新上传！'
+                    });
+                }
+            } else {
+                this.$message({
+                    type: 'warning',
+                    message: '请上传文件！'
                 });
+            }
         },
     },
 };
 </script>
-
