0b93fd9b4667b7d153e7851ac14f098f64d4dec1
bailingnan <lingnanbai@gmail.com>
2024-03-06 18:24:36 +0800

    feat:项目支出添加三级科目字段

--- ruoyi-system/src/main/java/com/ruoyi/project/mapper/ProjectExpenditureMapper.java
+++ ruoyi-system/src/main/java/com/ruoyi/project/mapper/ProjectExpenditureMapper.java
@@ -7,9 +7,8 @@ import org.apache.ibatis.annotations.Mapper;
 /**
  * @author bailingnan
  * @description 针对表【project_expenditure(项目支出表)】的数据库操作Mapper
- * @createDate 2024-02-23 10:39:52
+ * @createDate 2024-03-06 18:09:32
  * @Entity com.ruoyi.project.domain.ProjectExpenditure
- * @date 2024/02/23
  */
 @Mapper
 public interface ProjectExpenditureMapper
