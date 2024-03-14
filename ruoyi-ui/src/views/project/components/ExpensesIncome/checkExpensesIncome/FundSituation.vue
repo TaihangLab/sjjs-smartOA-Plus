7b80042cb979c086b8fe5cea9b9f5d0751eeeb57
xz <xz96424@163.com>
2024-03-14 15:24:53 +0800

    其他界面

--- ruoyi-ui/src/views/project/components/ExpensesIncome/checkExpensesIncome/FundSituation.vue
+++ ruoyi-ui/src/views/project/components/ExpensesIncome/checkExpensesIncome/FundSituation.vue
@@ -1,18 +1,10 @@
 <template>
     <div>
         <div style="margin-top: 10px;"></div>
-        <el-table
-            @row-mouseenter="handleRowMouseEnter"
-            @row-mouseleave="handleRowMouseLeave"
-            :row-class-name="getRowClassName"
-            :data="tableDataList"
-            style="width: 100%;margin-bottom: 20px;"
-            height="550px"
-            row-key="id"
-            border
-            :cell-style="columnStyle"
-            :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
-            highlight-current-row>
+        <el-table @row-mouseenter="handleRowMouseEnter" @row-mouseleave="handleRowMouseLeave"
+            :row-class-name="getRowClassName" :data="tableDataList" style="width: 100%;margin-bottom: 20px;"
+            height="550px" row-key="id" border :cell-style="columnStyle"
+            :tree-props="{ children: 'children', hasChildren: 'hasChildren' }" highlight-current-row>
             <el-table-column prop="label" label="预算科目名称" sortable width="180"></el-table-column>
             <el-table-column align="center" label="预算">
                 <el-table-column align="center" label="合计" prop="budget"></el-table-column>
@@ -30,7 +22,7 @@
 </template>
 
 <script>
-import {categoryOptions3} from "@/views/project/components/fundkeys";
+import { categoryOptions3 } from "@/views/project/components/fundkeys";
 
 export default {
     data() {
@@ -41,19 +33,19 @@ export default {
     },
     methods: {
         columnStyle({ row, column, rowIndex, columnIndex }) {
-	          if (columnIndex === 0) {
-                  return "background:#f3f5f8";
-		      } else if (columnIndex === 1) {
-                  return "background:#e2e280";
-              } else if (columnIndex === 2 || columnIndex === 3) {
-                  return "background:#c9db7e";
-              } else if (columnIndex === 4 || columnIndex === 5) {
-                  return "background:#aed37f";
-              } else if (columnIndex === 6 || columnIndex === 7 ) {
-                  return "background:#96cb7e";
-              } else if (columnIndex === 8 || columnIndex === 9 ) {
-                  return "background:#7bc37b";
-              }
+            if (columnIndex === 0) {
+                return "background:#f3f5f8";
+            } else if (columnIndex === 1) {
+                return "background:#e2e280";
+            } else if (columnIndex === 2 || columnIndex === 3) {
+                return "background:#c9db7e";
+            } else if (columnIndex === 4 || columnIndex === 5) {
+                return "background:#aed37f";
+            } else if (columnIndex === 6 || columnIndex === 7) {
+                return "background:#96cb7e";
+            } else if (columnIndex === 8 || columnIndex === 9) {
+                return "background:#7bc37b";
+            }
         },
         handleRowMouseEnter(row, event, rowIndex) {
             this.hoverRowIndex = rowIndex;
@@ -61,7 +53,7 @@ export default {
         handleRowMouseLeave() {
             this.hoverRowIndex = -1;
         },
-        getRowClassName({rowIndex}) {
+        getRowClassName({ rowIndex }) {
             if (rowIndex === this.hoverRowIndex) {
                 return 'row-hover';
             }
@@ -72,6 +64,12 @@ export default {
 </script>
 <style scoped>
 .row-hover {
-    background-color: #eef1f6 !important; /* 悬浮行的背景色 */
+    background-color: #eef1f6 !important;
+    /* 悬浮行的背景色 */
+}
+
+::v-deep .el-table__body tr.current-row>td {
+    background-color: rgb(44, 133, 44) !important;
+    color: #eef1f6
 }
 </style>
