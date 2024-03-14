8c1dd9c4a64dd1e9073d0dd12096c7f0d9ba9e07
zhangzhen <1749750148@qq.com>
2024-03-14 08:59:16 +0800

    修改、删除经费

--- ruoyi-ui/src/views/project/components/ExpensesIncome/checkExpensesIncome/CheckAppropriationAccount.vue
+++ ruoyi-ui/src/views/project/components/ExpensesIncome/checkExpensesIncome/CheckAppropriationAccount.vue
@@ -2,11 +2,11 @@
     <div>
         <div style="margin-top: 10px;"></div>
         <el-table v-loading="loading" :data="this.$props.appropriationAccount" style=" margin: 0; padding: 0;">
-            <el-table-column label="拨款金额" align="center" prop="amountReceived" :show-overflow-tooltip="true" width="150"/>
-            <el-table-column label="到账类型" align="center" prop="receivedType" :formatter="paymentType" :show-overflow-tooltip="true" width="100"/>
-            <el-table-column label="来款单位" align="center" prop="receivedFrom" :show-overflow-tooltip="true" width="150"/>
-            <el-table-column label="到账时间" align="center" prop="receivedDate" :show-overflow-tooltip="true" width="150"/>
-            <el-table-column label="附件" align="center" :show-overflow-tooltip="true" width="250">
+            <el-table-column label="拨款金额" align="center" prop="amountReceived" :show-overflow-tooltip="true" />
+            <el-table-column label="到账类型" align="center" prop="receivedType" :formatter="paymentType" :show-overflow-tooltip="true" />
+            <el-table-column label="来款单位" align="center" prop="receivedFrom" :show-overflow-tooltip="true" />
+            <el-table-column label="到账时间" align="center" prop="receivedDate" :show-overflow-tooltip="true" />
+            <el-table-column label="附件" align="center" :show-overflow-tooltip="true" >
                 <template slot-scope="scope" >
                     <div v-for="file in scope.row.sysOsses" :key="file.ossId">
                         <el-button size="mini" type="text" href="#" @click="downloadFile(file)">{{ file.originalName }}</el-button>
