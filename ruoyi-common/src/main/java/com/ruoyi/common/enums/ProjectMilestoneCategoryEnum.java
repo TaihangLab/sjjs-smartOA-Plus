38e26ec975cdc002655581744fdafb2bff9bee3b
rongtryhard <1130156344@qq.com>
2024-03-15 10:32:48 +0800

    feat:修改了大事记分类枚举类属性

--- ruoyi-common/src/main/java/com/ruoyi/common/enums/ProjectMilestoneCategoryEnum.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/enums/ProjectMilestoneCategoryEnum.java
@@ -10,52 +10,51 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ProjectMilestoneCategoryEnum implements IEnum<Integer> {
 
-    申报书(1,"申报书"),
+    DECLARATION(1,"申报书"),
 
-    任务书(2,"任务书"),
+    ASSIGNMENT(2,"任务书"),
 
-    科研协作合同(3,"科研协作合同"),
+    RESEARCH_COLLABORATION(3,"科研协作合同"),
 
-    专项经费相关文件(4,"专项经费相关文件"),
+    SPECIAL_FUNDS(4,"专项经费相关文件"),
 
-    经费管理表(5,"经费管理表"),
+    FUND_MANAGEMENT(5,"经费管理表"),
 
-    中期文件(6,"中期文件"),
+    MID_TERM_DOCUMENT(6,"中期文件"),
 
-    验收文件(7,"验收文件"),
+    ACCEPTANCE_DOCUMENTS(7,"验收文件"),
 
-    结题文件(8,"结题文件"),
+    CONCLUSION_DOCUMENT(8,"结题文件"),
 
-    知识产权(9,"知识产权"),
+    INTELLECTUAL_PROPERTY(9,"知识产权"),
 
-    论文(10,"论文"),
+    PAPER(10,"论文"),
 
-    专利(11,"专利"),
+    PATENT(11,"专利"),
 
-    软著(12,"软著"),
+    SOFTNESS(12,"软著"),
 
-    标准(13,"标准"),
+    STANDARD(13,"标准"),
 
-    示范应用(14,"示范应用"),
+    DEMONSTRATION_APPLICATION(14,"示范应用"),
 
-    获奖(15,"获奖"),
+    AWARDS(15,"获奖"),
 
-    报告(16,"报告"),
+    REPORT(16,"报告"),
 
-    专家咨询(17,"专家咨询"),
+    EXPERT_CONSULTATION(17,"专家咨询"),
 
-    经费变更(18,"经费变更"),
+    CHANGES_IN_FUNDING(18,"经费变更"),
 
-    人员变更(19,"人员变更"),
+    PERSONNEL_CHANGES(19,"人员变更"),
 
-    批复文件(20,"批复文件"),
+    APPROVAL_DOCUMENTS(20,"批复文件"),
 
-    通知(21,"通知"),
+    NOTICE(21,"通知"),
 
-    合同(22,"合同"),
+    CONTRACT(22,"合同"),
 
-    其它(0,"其它")
-
+    OTHER(0,"其它")
     ;
 
     @EnumValue
